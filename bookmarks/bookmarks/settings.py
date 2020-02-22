2dfe2a5a4169ecdf524375393dc4db6e5c03b9dc
Alex C-G <alexcg@outlook.com>
2020-02-22 17:26:30 +1300

    Use default login/logout methods

--- bookmarks/bookmarks/settings.py
+++ bookmarks/bookmarks/settings.py
@@ -31,14 +31,14 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'account.apps.AccountConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'account.apps.AccountConfig',
-    'bootstrap4'
+    'bootstrap4',
 ]
 
 MIDDLEWARE = [
